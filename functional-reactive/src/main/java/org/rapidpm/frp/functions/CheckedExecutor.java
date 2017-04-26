6c5e91ab2a4d64b6ae8d8a56d757ca5a5d47653f
Sven Ruppert <sven.ruppert@gmail.com>
2017-04-26 09:03:32 +0300

    fixed the execution path

--- functional-reactive/src/main/java/org/rapidpm/frp/functions/CheckedExecutor.java
+++ functional-reactive/src/main/java/org/rapidpm/frp/functions/CheckedExecutor.java
@@ -1,15 +1,16 @@
 package org.rapidpm.frp.functions;
 
+import org.rapidpm.frp.model.Result;
+
 /**
  * Created by svenruppert on 25.04.17.
  */
 @FunctionalInterface
 public interface CheckedExecutor extends CheckedConsumer<Void> {
 
-  void execute() throws Exception;
-
-  default Void applyWithException(Void v) throws Exception {
-    execute();
-    return null;
+  default Result<Void> execute() throws Exception {
+    return apply(null);
   }
+
+  Void applyWithException(Void v) throws Exception;
 }
