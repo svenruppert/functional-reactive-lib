16459a1b1ab43e0375509215ede7750bdeadc422
Sven Ruppert <sven.ruppert@gmail.com>
2018-09-29 22:39:45 +0200

    prepare release 00.07.05-RPM

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Copyright © 2013 Sven Ruppert (sven.ruppert@gmail.com)
+# Copyright © 2017 Sven Ruppert (sven.ruppert@gmail.com)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
